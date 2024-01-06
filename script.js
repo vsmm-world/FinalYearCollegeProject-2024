1604f51baa5e32f1646d3ae9f85871dd4f2fc95b
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-01-06 22:51:22 +0530

    added index

---
+++ script.js
@@ -0,0 +1,31 @@
+let upper = document.getElementById('btn1');
+let lower = document.getElementById('btn2');
+let remove = document.getElementById('btn3');
+let clear = document.getElementById('btn4');
+let copy = document.getElementById('btn5');
+let text = document.getElementById('text')
+
+upper.onclick = function() {
+    let uppperString = text.value.toUpperCase();
+    text.value = uppperString;
+}
+lower.onclick = function(){
+    let lowercase = text.value.toLowerCase()
+    text.value = lowercase;
+}
+clear.onclick= function(){
+    let clr = text.innerText = "";
+    text.value = clr;
+}
+copy.onclick = function(){
+    let copytext = navigator.clipboard.readText(text.value);
+    text = copytext;
+    // props.showalert("Copied to Clipboard", "success");
+}
+remove.onclick = function(){
+    let newText = text.value.split(/[ ]+/);
+    
+    text.value = newText;
+    
+    // props.showalert("Remove Extra-Spaces", "success")
+}
