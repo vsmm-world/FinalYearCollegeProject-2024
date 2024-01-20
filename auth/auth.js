59468dce5a272af5ef42028fad96b7eb09c65ca9
Ravindra Valand ᴿⱽ <67431996+vsmm-world@users.noreply.github.com>
2024-01-20 17:50:09 +0000

    trying to fix logic

--- auth/auth.js
+++ auth/auth.js
@@ -7,31 +7,37 @@ const register = async (req, res, next) => {
 
 console.log(req.body)
 
-    await bcrypt.hash(password, 10, async (err, hash) => {
-        if (err) {
-          console.log(err);
-        }
-        console.log(err)
-        console.log(hash);
-        let user = new User({
-            name,
-            username,
-            email,
-            password: hash
-        })
-        try {
-            const savedUser = await user.save();
-            res.status(200).json({
-                message: 'User added successfully',
-                user: savedUser
-            })
-        } catch (error) {
-            res.status(200).json({
-                message: 'An error occured'
-            })
-        }
-    }
-    )
+//    const  salt = await bcrypt.genSalt(10);
+    // const hash =  bcrypt.hash(password, 10);
+    const hash = await bcrypt.hashSync(password, 10);
+    // console.log(salt)
+    console.log( hash)
+
+    // await bcrypt.hash(password, 10, async (err, hash) => {
+    //     if (err) {
+    //       console.log(err);
+    //     }
+    //     console.log(err)
+    //     console.log(hash);
+    //     let user = new User({
+    //         name,
+    //         username,
+    //         email,
+    //         password: hash
+    //     })
+    //     try {
+    //         const savedUser = await user.save();
+    //         res.status(200).json({
+    //             message: 'User added successfully',
+    //             user: savedUser
+    //         })
+    //     } catch (error) {
+    //         res.status(200).json({
+    //             message: 'An error occured'
+    //         })
+    //     }
+    // }
+    // )
 }
 module.exports = register;
 
