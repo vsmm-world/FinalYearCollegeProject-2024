91677cf8d5c660ddee195de510f13d9d713efb76
Ravindra Valand ᴿⱽ <67431996+vsmm-world@users.noreply.github.com>
2024-01-20 17:51:47 +0000

    logic fix pending

--- auth/auth.js
+++ auth/auth.js
@@ -5,42 +5,42 @@ const register = async (req, res, next) => {
 
     const { name, username, email, password } = req.body;
 
-console.log(req.body)
+    console.log(req.body)
 
-//    const  salt = await bcrypt.genSalt(10);
+    //    const  salt = await bcrypt.genSalt(10);
     // const hash =  bcrypt.hash(password, 10);
     const hash = await bcrypt.hashSync(password, 10);
     // console.log(salt)
-    console.log( hash)
+    console.log(hash)
 
 
 
-    
-    // await bcrypt.hash(password, 10, async (err, hash) => {
-    //     if (err) {
-    //       console.log(err);
-    //     }
-    //     console.log(err)
-    //     console.log(hash);
-    //     let user = new User({
-    //         name,
-    //         username,
-    //         email,
-    //         password: hash
-    //     })
-    //     try {
-    //         const savedUser = await user.save();
-    //         res.status(200).json({
-    //             message: 'User added successfully',
-    //             user: savedUser
-    //         })
-    //     } catch (error) {
-    //         res.status(200).json({
-    //             message: 'An error occured'
-    //         })
-    //     }
-    // }
-    // )
+
+    await bcrypt.hash(password, 10, async (err, hash) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(err)
+        console.log(hash);
+        let user = new User({
+            name,
+            username,
+            email,
+            password: hash
+        })
+        try {
+            const savedUser = await user.save();
+            res.status(200).json({
+                message: 'User added successfully',
+                user: savedUser
+            })
+        } catch (error) {
+            res.status(200).json({
+                message: 'An error occured'
+            })
+        }
+    }
+    )
 }
 module.exports = register;
 
