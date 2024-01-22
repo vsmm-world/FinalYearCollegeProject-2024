495a29702b99ffb3c4b937c52f686d738d38dfaf
Ravindra Valand ᴿⱽ <67431996+vsmm-world@users.noreply.github.com>
2024-01-22 17:22:25 +0000

    final fixes in register

--- auth/auth.js
+++ auth/auth.js
@@ -1,34 +1,34 @@
+const { default: mongoose } = require('mongoose');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res, next) => {
-    try {
-        const { name, username, email, Password } = req.body;
 
-        console.log(req.body);
-
-        // Use bcrypt.hash with promises
-        const hash = await bcrypt.hash(Password, 10);
-
-        console.log(hash);
-
-        let user = new User({
-            name,
-            username,
-            email,
-            password: hash,
-        });
-        const savedUser = await user.save();
-        res.status(200).json({
-            message: 'User added successfully',
-            user: savedUser,
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            message: 'An error occurred',
+    const { name, username, email, Password } = req.body;
+    const chek = await User.findOne({ email: email });
+    // console.log(chek)
+    if (chek) {
+        return res.status(400).json({
+            message: 'Email already exists',
         });
     }
+
+
+    const hash = await bcrypt.hash(Password, 10);
+
+
+    let user = new User({
+        name,
+        username,
+        email,
+        password: hash,
+    });
+    const savedUser = await user.save();
+    res.status(200).json({
+        message: 'User added successfully',
+        user: savedUser,
+    });
+
 };
 
 module.exports = register;
