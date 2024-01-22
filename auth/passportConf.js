d65c65072d2aea0c8423cd87e00f64d40fd5ccdc
Ravindra Valand ᴿⱽ <67431996+vsmm-world@users.noreply.github.com>
2024-01-22 17:37:56 +0000

    updating the format

--- auth/passportConf.js
+++ auth/passportConf.js
@@ -4,40 +4,41 @@ const bcrypt = require("bcryptjs");
 const LocalStrategy = require("passport-local").Strategy;
 
 const PassInit = (passport) => {
-  passport.use(
-    new LocalStrategy(async (username, password, done) => {
-      try {
-        const user = await User.findOne({ username });
-        if (!user) return done(null, false);
-        bcrypt.compare(password, user.password).then((result) => {
-          if (!result) {
-            return done(null, false);
-          }
-        });
-        return done(null, user);
-      } catch (error) {
-        return done(error, false);
-      }
-    })
-  );
+    passport.use(
+        new LocalStrategy(async (username, password, done) => {
+            try {
+                const user = await User.findOne({ username });
+                if (!user) return done(null, false);
+                bcrypt.compare(password, user.password).then((result) => {
+                    if (!result) {
+                        return done(null, false);
+                    }
+                });
+                return done(null, user);
+            } catch (error) {
+                return done(error, false);
+            }
+        })
+    );
 
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
+    passport.serializeUser((user, done) => {
 
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const user = await User.findById(id);
-      return done(null, user);
-    } catch (error) {
-      return done(error, false);
-    }
-  });
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);
+            return done(null, user);
+        } catch (error) {
+            return done(error, false);
+        }
+    });
 };
 
 const isAuthenticted = (req, res, next) => {
-  if (req.user) return next();
-  res.redirect("/login");
+    if (req.user) return next();
+    res.redirect("/login");
 };
 
 module.exports = { PassInit, isAuthenticted };
