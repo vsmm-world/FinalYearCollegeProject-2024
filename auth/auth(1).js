ab0087dee3432920a4088e5a810f38d02eb6595d
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-02-02 11:16:20 +0530

    changed

---
+++ auth/auth(1).js
@@ -0,0 +1,25 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+const register = async (req, res, next) => {
+
+    const { name, username, email, password } = req.body;
+
+    bcrypt.hash(password, 10).then(async (hash) => {
+        await User.create({
+            name,
+            username,
+            email,
+            password:hash,
+        }).then((e) => {
+
+            res.status(200).json({ message: "Succsess" });
+            console.log(e);
+        }).catch(() => {
+            res.status(400).json({
+                message: 'Not Registerd'
+            })
+        })
+    })
+}
+module.exports = register;
---
+++ db/connection(1).js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const uri = process.env.MONOG_DB_URI || 'mongodb+srv://ravindra:rvrvrv@cluster0.y3ybjan.mongodb.net/?retryWrites=true&w=majority'
+
+const ConnectDB = async () => {
+    await mongoose.connect(uri).then(() => {
+        console.log('Connected to Database');
+    })
+}
+module.exports = ConnectDB;
