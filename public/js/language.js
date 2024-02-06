f024d210ed4cbc0e625a1bdaf179013969c3c46a
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-02-06 15:51:04 +0530

    contact page

--- public/js/language.js
+++ public/js/language.js
@@ -1,8 +1,8 @@
-item = document.querySelector(".item-ul").querySelectorAll("a");
-console.log(item)
-item.forEach(element => {element.addEventListener("click" , () =>{
-        item.forEach(list => list.classList.remove(".active"))
-        this.classList.add(".active")
-    })
-})
+// item = document.querySelectorAll(".item-btn");
+// console.log(item)
+// item.forEach(element => {element.addEventListener("click" , () =>{
+//         item.forEach(list => list.classList.remove(".active"))
+//         this.classList.add(".active")
+//     })
+// })
 
