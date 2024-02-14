7db93e11b026ec36fed5bec8c3c18b78f73a32f8
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-02-14 17:46:21 +0530

    sm changes

--- public/js/mode.js
+++ public/js/mode.js
@@ -6,13 +6,13 @@ let toggle = true;
 mode.addEventListener("click" , () => {
     toggle = !toggle;
     if(toggle){
-        mode.src = "/icons/moon.png";
+        mode.src = "/icons/sun.png";
         root.style.setProperty("--light" , "#ADEFD1FF")
         root.style.setProperty("--dark" , "#00203FFF")
         root.style.setProperty("--backphoto" , `url("/img/wal1.jpg")`)
     }
     else{
-        mode.src = "/icons/sun.png";
+        mode.src = "/icons/moon.png";
         root.style.setProperty("--light" , "rgb(126, 116, 116)")
         root.style.setProperty("--dark" , "rgb(37, 35, 35)")
         root.style.setProperty("--backphoto" , "#FFFFFF")
