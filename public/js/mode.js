3c31c2c9e4f34941b34c66201948293ac5c2269d
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-02-15 16:32:26 +0530

    dark mode

--- public/js/mode.js
+++ public/js/mode.js
@@ -3,18 +3,25 @@ let root = document.documentElement;
 console.log(root.style.setProperty);
 console.log(mode.src);
 let toggle = true;
-mode.addEventListener("click" , () => {
+if(mode.src = "/icons/sun.png"){
+    document.body.classList.toggle("dark")
+}
+else if(mode.src = "/icons/moon.png"){
+    document.body.classList.toggle("light")
+}
+const modechange = function (event){
+    event.preventDefault()
     toggle = !toggle;
     if(toggle){
         mode.src = "/icons/sun.png";
-        root.style.setProperty("--light" , "#ADEFD1FF")
-        root.style.setProperty("--dark" , "#00203FFF")
-        root.style.setProperty("--backphoto" , `url("/img/wal1.jpg")`)
+        document.body.classList.add("dark")
+        document.body.classList.remove("light")
     }
     else{
         mode.src = "/icons/moon.png";
-        root.style.setProperty("--light" , "rgb(126, 116, 116)")
-        root.style.setProperty("--dark" , "rgb(37, 35, 35)")
-        root.style.setProperty("--backphoto" , "#FFFFFF")
+        document.body.classList.add("light")
+        document.body.classList.remove("dark")
+
     }
-})
+}
+mode.addEventListener("click" , modechange)
